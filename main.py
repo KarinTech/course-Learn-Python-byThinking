359dc4e798ae8886c0a90baee4a89d39219cd8ae
Karin Centenaro <>
2022-10-08 15:55:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+name="Andres"
+age=8
+year_of_birth=2015
+name2="Karin"
+age2=46
+year_of_birth2=1975
+print((name),(age),(year_of_birth),(name2),(age2),(year_of_birth2))
+age_in_ten_y=age +10
+print(age_in_ten_y) 
+age_in_ten_y2=(age2+10) 
+print(age_in_ten_y2)
+
